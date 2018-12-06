239855ca6e3b2b3ce644b717c4c00fe3c99d6911
Jakob Sahlström <jakob.sahlstrom@klarna.com>
2018-12-06 21:01:37 +0100

    Add Day 2 skeleton

---
+++ src/main/scala/advent/Day4.scala
@@ -0,0 +1,6 @@
+package advent
+
+object Day4 extends Puzzle[Int, Int] {
+  def partA(input: List[String]): Int = ???
+  def partB(input: List[String]): Int = ???
+}
---
+++ src/test/scala/advent/Day4Spec.scala
@@ -0,0 +1,23 @@
+package advent
+
+import advent.util._
+import org.scalatest._
+
+class Day4Spec extends FlatSpec with Matchers {
+
+  import Day4Spec._
+
+  "Day4" should "solve first part of the puzzle" in pending
+
+  it should "solve second part of the puzzle" in pending
+}
+
+object Day4Spec {
+  val rawInput =
+    """
+      |
+    """.stripMargin
+}
+
+
+
