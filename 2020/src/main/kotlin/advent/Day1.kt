27dbf58e2d21b6dd410734b2cec2a867ad2160f2
Jakob Horndahl <jakob.horndahl@mojang.com>
2020-12-01 11:41:47 +0100

    Day 1 solution

--- 2020/src/main/kotlin/advent/Day1.kt
+++ 2020/src/main/kotlin/advent/Day1.kt
@@ -1,11 +1,53 @@
 package advent
 
 object Day1 : Puzzle<Int, Int> {
+
+    const val addsUpTo = 2020
+    const val midway = addsUpTo / 2
+
     override fun partA(input: List<String>): Int {
-        TODO()
+        return parseInput(input)
+                .let(this::findPair)
+                .also { println("Found $it") }
+                .let { (i1, i2) -> i1 * i2 }
     }
 
     override fun partB(input: List<String>): Int {
-        TODO()
+        return parseInput(input)
+                .let(this::findTriplet)
+                .also { println("Found $it") }
+                .let { (i1, i2, i3) -> i1 * i2 * i3 }
+    }
+
+    private fun findPair(input: List<Int>): Pair<Int, Int> {
+        fun findPair(source: List<Int>, target: List<Int>): Pair<Int, Int>? {
+            return source.flatMap { s -> target.map { Pair(s, it) } }
+                    .find { (i1, i2) -> i1 + i2 == addsUpTo }
+        }
+        val (lower, upper) = input.partition { it < midway }
+
+        return findPair(lower, upper) ?: error("No pair found")
+    }
+
+    private fun findTriplet(input: List<Int>): Triple<Int, Int, Int> {
+        val sorted = input.sorted()
+        fun valuesInRange(i: Int): List<Int> {
+            return sorted.filter { it < i }
+        }
+
+        return sorted
+                .flatMap { i1 ->
+                    valuesInRange(i1)
+                            .flatMap { i2 ->
+                                valuesInRange(i1 + i2)
+                                        .map { i3 -> Triple(i1, i2, i3) }
+                            }
+                }
+                .find { (i1, i2, i3) -> i1 + i2 + i3 == addsUpTo }
+                ?: error("No triplet found")
+    }
+
+    private fun parseInput(input: List<String>): List<Int> {
+        return input.map(Integer::valueOf)
     }
 }
--- 2020/src/test/kotlin/advent/Day1Test.kt
+++ 2020/src/test/kotlin/advent/Day1Test.kt
@@ -1,6 +1,39 @@
 package advent
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class Day1Test : PuzzleRunner<Int, Int>() {
     override val input = javaClass.getResourceAsStream("/Day1.txt").bufferedReader().readLines();
     override val puzzle = Day1
+
+    @Test
+    fun `Part A example`() {
+        val exampleInput = """
+            1721
+            979
+            366
+            299
+            675
+            1456
+        """.trimIndent()
+        val correctAnswer = 514579
+        val actualAnswer = Day1.partA(exampleInput.lines())
+        assertEquals(correctAnswer, actualAnswer)
+    }
+
+    @Test
+    fun `Part B example`() {
+        val exampleInput = """
+            1721
+            979
+            366
+            299
+            675
+            1456
+        """.trimIndent()
+        val correctAnswer = 241861950
+        val actualAnswer = Day1.partB(exampleInput.lines())
+        assertEquals(correctAnswer, actualAnswer)
+    }
 }
